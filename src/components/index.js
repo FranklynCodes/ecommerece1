e00ca331979e30f057dafda4451fb7c8face88a2
FranklynCodes <47542708+FranklynCodes@users.noreply.github.com>
2021-07-30 01:55:13 -0400

    Added Checkout/Button + | Page w/Steppers

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as NavBar } from "./Navbar/Navbar";
 export { default as Products } from "./Products/Products";
 export { default as Cart } from "./Cart/Cart";
-
+export { default as Checkout } from "./CheckoutForm/Checkout/Checkout";
 // Condences the imports and exports

173c97373ea5a3c26e6a6f35e2621d04028ebb9f
Franklyn-C <47542708+Franklyn-C@users.noreply.github.com>
2021-07-30 22:09:37 -0400

    React Form Fixed Added Render Method to Controller

--- src/components/CheckoutForm/Checkout/CustomTextField.jsx
+++ src/components/CheckoutForm/Checkout/CustomTextField.jsx
@@ -9,7 +9,7 @@ export default function FormInput({ name, label, required }) {
 
     return (
         <Grid item xs={12} sm={6}>
-            <Controller as={TextField} control={control} fullWidth name={name} label={label} required={required}></Controller>
+            <Controller control={control} name={name} render={({ field }) => <TextField fullWidth label={label} required />}></Controller>
         </Grid>
     );
 }

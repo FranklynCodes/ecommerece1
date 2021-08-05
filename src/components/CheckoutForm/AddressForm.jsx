a944f314a035bf6d064df31e2eea45c905212ad9
Franklyn-C <47542708+Franklyn-C@users.noreply.github.com>
2021-08-04 23:29:33 -0400

    Shipping Country Maping Object

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,32 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
-import { useForm, FormProvider } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+import { Link } from "react-router-dom";
 
-import { commerce } from '../../lib/commerce';
-import FormInput from './CustomTextField';
+import { commerce } from "../../lib/commerce";
+import FormInput from "./CustomTextField";
 
 // import { Checkout } from "@chec/commerce.js/features/checkout";
 
 // Connecting ReactHooks to MaterialUI text input
 export default function AddressForm({ checkoutToken }) {
-    console.log('checkoutToken:', checkoutToken)    
-    console.log('checkoutToken.id:', checkoutToken.id)
+    console.log("checkoutToken:", checkoutToken);
+    console.log("checkoutToken.id:", checkoutToken.id);
     // Create State varibles to store commerece api variables
     const [shippingCountries, setShippingCountries] = useState([]);
-    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingCountry, setShippingCountry] = useState("");
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
-    const [shippingSubdivision, setShippingSubdivision] = useState('');
+    const [shippingSubdivision, setShippingSubdivision] = useState("");
     const [shippingOptions, setShippingOptions] = useState([]);
-    const [shippingOption, setShippingOption] = useState('');
-    
+    const [shippingOption, setShippingOption] = useState("");
 
     // Recipe ID = checkoutTokenId
     // console.log('checkoutTokenId:', checkoutToken)
     const fetchShippingCountries = async (checkoutTokenId) => {
         const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
         console.log("countries:", countries);
-        setShippingCountries(countries);
+        setShippingCountries(countries); 
+        // countries is a object, so we cannot access it normally like a array, and we can't use keys individually, want it to dynamically update 
+
+        setShippingCountry(Object.keys(countries)[0]);
     };
 
     useEffect(() => {
@@ -51,15 +53,18 @@ export default function AddressForm({ checkoutToken }) {
                         <FormInput require name="city" label="City"></FormInput>
                         <FormInput require name="zip" label="Zip / Postal code"></FormInput>
                         {/* Wrap this Grid  */}
-                        {/* <Grid item xs={12} sm = {6}>
+                        <Grid item xs={12} sm = {6}>
                             <InputLabel>Shipping Country </InputLabel>
-                            <Select value={""} fullWidth onChange={}>
-                                <MenuItem key={} value={}>
+                            <Select value={shippingCountry} fullWidth onChange={(e)=> setShippingCountry(e.target.value)}>
+                                {/* Below is a array of arrays  */}
+                                {Object.entries(shippingCountries)}
+                                {console.log('Object.entries(shippingCountries):', Object.entries(shippingCountries))}
+                                {/* <MenuItem key={} value={}>
                                     Select Me 
-                                </MenuItem>                                
+                                </MenuItem>                                 */}
                             </Select>
                         </Grid>
-                        <Grid item xs={12} sm = {6}>
+                        {/* <Grid item xs={12} sm = {6}>
                             <InputLabel>Shipping Subdivision </InputLabel>
                             <Select value={""} fullWidth onChange={}>
                                 <MenuItem key={} value={}>
